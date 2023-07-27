8419746f57cd544520b41655ef4905e325f9608b
jxy y <thushar17223@gmail.com>
2023-07-27 15:02:07 +0530

    guvbidfs

--- backend-php/connect.php
+++ backend-php/connect.php
@@ -14,3 +14,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+?>
--- backend-php/student-login.php
+++ backend-php/student-login.php
@@ -3,40 +3,30 @@
     $email = $password = "";
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Get user input and perform basic validation/sanitization
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        // Prepare the SQL statement using placeholders
-        $sql = "SELECT * FROM registered_users WHERE email = '$email' AND password = '$password'";
+        $sql = "SELECT * FROM registerd_users WHERE email_adress = ? AND password = ?";
         $stmt = $conn->prepare($sql);
 
         if (!$stmt) {
-            // Check if the statement preparation failed
             echo "Error: " . $conn->error;
             exit();
         }
 
-        // Bind the parameters
         $stmt->bind_param("ss", $email, $password);
-
-        // Execute the query
         $stmt->execute();
-
-        // Store the result
         $result = $stmt->get_result();
 
-        // Check if a row was returned (successful login)
         if ($result->num_rows === 1) {
             // User exists, redirect to home.html
-            header('test.html');
-            exit(); // Make sure to exit after a redirect
+            header('Location: test.html');
+            exit();
         } else {
             // User does not exist or wrong credentials
             echo "Invalid email or password.";
         }
 
-        // Close the statement and result set
         $stmt->close();
         $result->close();
     } 
