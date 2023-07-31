9289840b873c4f6370b0fc709acac3c5b5cc9720
jxy y <thushar17223@gmail.com>
2023-07-31 10:44:51 +0530

    Updated and deleted alot of things.

---
+++ js/applies.js
@@ -0,0 +1,24 @@
+const Orders = [
+    {
+        nameofInstitute: 'College of Applied Science',
+        district: 'Pathanamthitta',
+        programmes: 'UG and PG', 
+        status: 'Pending'
+    },
+
+    {
+        nameofInstitute: 'MEA Engineering College',
+        district: 'Malappuram',
+        programmes: 'UG and Engineering', 
+        status: 'Declined'
+    },
+
+    {
+        nameofInstitute: 'Govt. College of Nursing, Thrissur',
+        district: 'Thrissur',
+        programmes: 'Nursing', 
+        status: 'Active'
+    },
+
+    
+]
