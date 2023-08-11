bff8256d7b0261aab4a630c790bd7982d190ecd5
jxytherealone <thushar17223@gmail.com>
2023-08-11 13:08:08 +0530

    some stupid spaces

--- backend-php/student-register.php
+++ backend-php/student-register.php
@@ -9,9 +9,9 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $dob = $_POST['dob'];
-    $formattedDob = date('Y-m-d', strtotime($_POST['dob']));
 
-    // Concatenate the day, month, and year to form a valid date format (YYYY-MM-DD)
+    //formatting syntax for date and time
+    $formattedDob = date('Y-m-d', strtotime($_POST['dob']));
     
 
     $sql = "INSERT INTO registered_users (firstname, lastname, email, password, dob) VALUES ('$firstname', '$lastname', '$email', '$password', '$formattedDob')";
