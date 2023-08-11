43fb9b49409513002ae5c5cf94d20682579fe639
jxytherealone <thushar17223@gmail.com>
2023-08-11 13:03:19 +0530

    fixed the day now uses primitive date types.

--- backend-php/student-register.php
+++ backend-php/student-register.php
@@ -1,21 +1,20 @@
 <?php 
   include('connect.php');
 
-  $id = $firstname = $lastname = $email = $password = $day = $month = $year = "";
+  $firstname = $lastname = $email = $password = $dob = "";
 
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $day = $_POST['day'];
-    $month = $_POST['month'];
-    $year = $_POST['year'];
+    $dob = $_POST['dob'];
+    $formattedDob = date('Y-m-d', strtotime($_POST['dob']));
 
     // Concatenate the day, month, and year to form a valid date format (YYYY-MM-DD)
     
 
-    $sql = "INSERT INTO registered_users (firstname, lastname, email, password, day, month, year) VALUES ('$firstname', '$lastname', '$email', '$password', $day, $month, $year)";
+    $sql = "INSERT INTO registered_users (firstname, lastname, email, password, dob) VALUES ('$firstname', '$lastname', '$email', '$password', '$formattedDob')";
     if ($conn->query($sql) === TRUE) {
       header('Location: ../../php-project/student-login.html');
     } else {
