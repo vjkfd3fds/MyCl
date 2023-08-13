ba7ed35f9e54fb1a5022a8803ebad5bca0f9c649
jxytherealone <thushar17223@gmail.com>
2023-08-13 14:39:26 +0530

    added a comma lol

--- backend-php/college-details-process.php
+++ backend-php/college-details-process.php
@@ -16,50 +16,50 @@
         $programs = '';
         
         if (isset($_POST['ug'])) {
-            $programs .= $_POST['ug'] . ' ';
+            $programs .= $_POST['ug'] . ' , ';
         } 
 
         if (isset($_POST['pg'])) {
-            $programs .= $_POST['pg'] . ' ';
+            $programs .= $_POST['pg'] . ' , ';
         }
         
         if (isset($_POST['diploma'])) {
-            $programs .= $_POST['diploma'] . ' ';
+            $programs .= $_POST['diploma'] . ' , ';
         } 
         
         if (isset($_POST['engineering'])) {
-            $programs .= $_POST['engineering'] . ' ';
+            $programs .= $_POST['engineering'] . ' , ';
         } 
         
         if (isset($_POST['phd'])) {
-            $programs .= $_POST['phd'] . ' ';
+            $programs .= $_POST['phd'] . ' , ';
         }  
         
         if (isset($_POST['nursing'])) {
-            $programs .= $_POST['nursing'] . ' ';
+            $programs .= $_POST['nursing'] . ' , ';
         } 
         
         if (isset($_POST['doctorate'])) {
-            $programs .= $_POST['doctorate'] . ' ';
+            $programs .= $_POST['doctorate'] . ' , ';
         }
 
         if (isset($_POST['ds'])) {
-            $programs .= $_POST['ds'] . ' ';
+            $programs .= $_POST['ds'] . ' , ';
         }
 
         if(isset( $_POST['moa'])) {
-            $programs .= $_POST['moa'] . ' ';
+            $programs .= $_POST['moa'] . ' , ';
         } 
         
         if (isset($_POST['doe'])) {
-            $programs .= $_POST['doe'] . ' ';
+            $programs .= $_POST['doe'] . ' , ';
         }  
         
         if (isset($_POST['ba-1'])) {
-            $programs .= $_POST['da-1'] . ' ';
+            $programs .= $_POST['da-1'] . ' , ';
         } 
         if (isset($_POST['bfa'])) {
-            $programs .= $_POST['bfa'] . ' ';
+            $programs .= $_POST['bfa'] . ' , ';
         }
 
 
