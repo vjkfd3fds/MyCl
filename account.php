227626d4aa79549772b7950b75212b5d6e8cdc1e
jxytherealone <thushar17223@gmail.com>
2023-08-28 12:30:59 +0530

    UMM

--- account.php
+++ account.php
@@ -18,7 +18,11 @@
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc(); 
                 ?>
-                    <input type="text" value="<?php echo $row['email'];?>" readonly>
+                    <input type="text" value="<?php echo $row['firstname'];?>" readonly>  <br/> <br/>
+                    <input type="text" value="<?php echo $row['lastname'];?>" readonly>  <br/> <br/>
+                    <input type="text" value="<?php echo $row['email'];?>" readonly>  <br/> <br/>
+                    <input type="text" value="<?php echo $row['password'];?>" readonly>  <br/> <br/>
+                    <input type="text" value="<?php echo $row['dob'];?>" readonly>  <br/> <br/>
             <?php
             } else {
                 echo "No user found.";
