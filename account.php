f7ff4e22e689c5c370672396c5eae5e422528e9e
jxytherealone <thushar17223@gmail.com>
2023-08-28 12:54:21 +0530

    HM

--- account.php
+++ account.php
@@ -17,12 +17,31 @@
             
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc(); 
-                ?>
-                    <input type="text" value="<?php echo $row['firstname'];?>" readonly>  <br/> <br/>
-                    <input type="text" value="<?php echo $row['lastname'];?>" readonly>  <br/> <br/>
-                    <input type="text" value="<?php echo $row['email'];?>" readonly>  <br/> <br/>
-                    <input type="text" value="<?php echo $row['password'];?>" readonly>  <br/> <br/>
-                    <input type="text" value="<?php echo $row['dob'];?>" readonly>  <br/> <br/>
+    ?>          
+                <form method="post" action="account.php">
+                    First Name: <input type="text" name="firstname" value="<?php echo $row['firstname'];?>" readonly>  <br/> <br/>
+                    Last Name: <input type="text" name="lastname" value="<?php echo $row['lastname'];?>" readonly>  <br/> <br/>
+                    Email: <input type="text" name="email" value="<?php echo $row['email'];?>" readonly>  <br/> <br/>
+                    Password: <input type="text" name="password" value="<?php echo $row['password'];?>" readonly>  <br/> <br/>
+                    Date Of Birth: <input type="text" name="dob" value="<?php echo $row['dob'];?>" readonly>  <br/> <br/>
+                </form>
+                <form method="post">
+                    <input type="text" name="firstname" placeholder="Enter the new first name">
+                    <button type="submit">Update</button>
+                </form>
+
+                    <?php 
+                        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                            $firstname = $_POST['firstname'];
+                            $email = $_COOKIE['email'];
+                            $sql = "UPDATE registered_users SET firstname = ? WHERE email = ?";
+                            $stmt = $conn->prepare($sql);
+                            $stmt->bind_param("ss", $firstname, $email);
+                            $stmt->execute();
+                            // Close the prepared statement
+                            $stmt->close();
+                        }
+                    ?>
             <?php
             } else {
                 echo "No user found.";
@@ -30,6 +49,6 @@
         }
 
         $conn->close();
-    ?>
+            ?>
 </body>
 </html>
