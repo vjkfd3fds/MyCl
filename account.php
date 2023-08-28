7e9702e92edc4a4ec75ec56dbad7a7d7b094e64b
jxytherealone <thushar17223@gmail.com>
2023-08-28 12:26:17 +0530

    i did something i dont even know bruh lmao

--- account.php
+++ account.php
@@ -6,6 +6,26 @@
     <title>MyCl | User Account</title>
 </head>
 <body>
-    
+    <?php 
+        include('backend-php/connect.php');
+       
+
+        if (isset($_COOKIE['email'])) {
+            $email = $_COOKIE['email'];
+            $sql = "SELECT firstname, lastname, email, password, dob FROM registered_users WHERE email = '$email'";
+            $result = $conn->query($sql);
+            
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc(); 
+                ?>
+                    <input type="text" value="<?php echo $row['email'];?>" readonly>
+            <?php
+            } else {
+                echo "No user found.";
+            }
+        }
+
+        $conn->close();
+    ?>
 </body>
 </html>
