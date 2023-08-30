08e96f1dcf1cfbcafc1b0af76b09326340161492
jxytherealone <thushar17223@gmail.com>
2023-08-30 19:30:22 +0530

    um

--- college-dashboard.php
+++ college-dashboard.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyCl | Dashboard</title>
     <link rel="icon" href="images/note.png">
+    <link rel="stylesheet" href="other-css/college-dashboard.css">
 </head>
 <body>
-    
+    <section class="section-1">
+        <p>Hello</p>
+    </section>
 </body>
 </html>
