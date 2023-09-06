915b05460f600beb7bc9a5f865e8505071159e5d
jxytherealone <thushar17223@gmail.com>
2023-09-06 11:01:07 +0530

    U

--- update.php
+++ update.php
@@ -7,8 +7,6 @@
 </head>
 <body>
 <form method="post">
-    New first name: <input type="text" name="firstname" placeholder="Enter the new first name"> <br/> <br/> 
-    New last name: <input type="text" name="lastname" placeholder="Enter the new last name"> <br/> <br/> 
     New password: <input type="text" name="password" placeholder="Enter the new password"> <br/> <br/> 
     Confirm new password: <input type="text" name="confirm" placeholder="re-enter the new password"> <br/><br/>
     
@@ -19,14 +17,12 @@
     include('backend-php/connect.php');
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
         $password = $_POST['password'];
         $email = $_COOKIE['email'];
 
-        $sql = "UPDATE registered_users SET firstname = ?, lastname = ?, password = ? WHERE email = ?";
+        $sql = "UPDATE registered_users SET password = ? WHERE email = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssss", $firstname, $lastname, $password, $email);
+        $stmt->bind_param("ss", $password, $email);
         $stmt->execute();
         $stmt->close();
     }
