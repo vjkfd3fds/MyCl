6bd52ba2f30711e5c8058c6dfe1925b158bcdcb0
jxytherealone <thushar17223@gmail.com>
2023-09-09 16:59:13 +0530

    f

--- requests.php
+++ requests.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MyCl | Requests</title>
     <link rel="icon" href="images/note.png">
     <style>
         .random-button {
