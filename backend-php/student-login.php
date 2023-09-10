17a35026c6090b2139d5b66acd642f4f78bfccd4
jxytherealone <thushar17223@gmail.com>
2023-09-10 09:14:59 +0530

    changed the things

--- backend-php/student-login.php
+++ backend-php/student-login.php
@@ -6,15 +6,16 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $sql = "SELECT * FROM registered_users WHERE email = ? AND password = ? AND id = ?";
+        $sql = "SELECT email, password, id FROM registered_users WHERE email = ? AND password = ?";
         $stmt = $conn->prepare($sql);
 
-
-        $stmt->bind_param("sss", $id, $email, $password);
+        $stmt->bind_param("ss", $email, $password);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $id = $row['id'];
             // User exists, redirect to home.html
             setcookie("id", $id, time() + 3600, "/");
             header('Location: ../../php-project/student-dashboard.php');
