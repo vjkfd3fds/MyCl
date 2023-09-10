48bbca300b77b5c172d52df3ca4b23ad15e7d202
jxytherealone <thushar17223@gmail.com>
2023-09-10 09:05:50 +0530

    .

--- backend-php/student-login.php
+++ backend-php/student-login.php
@@ -6,18 +6,17 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = $_POST['email'];
         $password = $_POST['password'];
-
-        $sql = "SELECT * FROM registered_users WHERE email = ? AND password = ?";
+        $sql = "SELECT * FROM registered_users WHERE email = ? AND password = ? AND id = ?";
         $stmt = $conn->prepare($sql);
 
 
-        $stmt->bind_param("ss", $email, $password);
+        $stmt->bind_param("sss", $id, $email, $password);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows === 1) {
             // User exists, redirect to home.html
-            setcookie("email", $email, time() + 3600, "/");
+            setcookie("id", $id, time() + 3600, "/");
             header('Location: ../../php-project/student-dashboard.php');
             exit();
         } else {
