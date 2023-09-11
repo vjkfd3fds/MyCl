598a9cf4ba7e179466dc4900685bbeb5549e1520
jxytherealone <thushar17223@gmail.com>
2023-09-11 14:56:59 +0530

    .

--- account.php
+++ account.php
@@ -1,10 +1,18 @@
 <head>
     <style>
         .no-class {
-            width: 200;
-            height: 200;
+            width: 100;
+            height: 100;
+        }
+        
+        .nav-bar {
+            border: 1px solid none; 
+            display: flex; 
+            justify-content: flex-end;
         }
     </style>
+    <link rel="icon" href="images/note.png">
+    <title>MyCl | Account</title>
 </head>
 
 <?php 
@@ -14,7 +22,7 @@
         $sql = "SELECT * FROM registered_users WHERE id = '$user_id'";
         $result = $conn->query($sql);
         if ($row = $result->fetch_assoc()) {
-            echo '<div style="border: 1px solid black; display: flex; justify-content: flex-end">';
+            echo '<div class="nav-bar">';
             echo '<h1 style="font-family: monospace; margin-top: 20px;"> Welcome ' . $row['firstname'] . '<h1>';
             ?>
             <img class="no-class" src="profile/<?php echo $row['profile']; ?>"
