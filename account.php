680691aed8cc196a9efb570e07cf09584b589ead
jxytherealone <thushar17223@gmail.com>
2023-09-11 14:52:50 +0530

    cats are ruining my life

--- account.php
+++ account.php
@@ -1,3 +1,12 @@
+<head>
+    <style>
+        .no-class {
+            width: 200;
+            height: 200;
+        }
+    </style>
+</head>
+
 <?php 
     include('backend-php/connect.php');
     if (isset($_COOKIE['id'])) {
@@ -7,6 +16,9 @@
         if ($row = $result->fetch_assoc()) {
             echo '<div style="border: 1px solid black; display: flex; justify-content: flex-end">';
             echo '<h1 style="font-family: monospace; margin-top: 20px;"> Welcome ' . $row['firstname'] . '<h1>';
+            ?>
+            <img class="no-class" src="profile/<?php echo $row['profile']; ?>"
+            <?php
             echo '</div>';
         }
     }
