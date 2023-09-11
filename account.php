b7105e627056e28314fa53e180d3249465ff8dd6
jxytherealone <thushar17223@gmail.com>
2023-09-11 14:41:01 +0530

    very cool

--- account.php
+++ account.php
@@ -5,7 +5,9 @@
         $sql = "SELECT * FROM registered_users WHERE id = '$user_id'";
         $result = $conn->query($sql);
         if ($row = $result->fetch_assoc()) {
-            echo '<a href=""> ' . $row['firstname'] . '</a>';
+            echo '<div style="border: 1px solid black; display: flex; justify-content: flex-end">';
+            echo '<h1 style="font-family: monospace; margin-top: 20px;"> Welcome ' . $row['firstname'] . '<h1>';
+            echo '</div>';
         }
     }
 ?>
