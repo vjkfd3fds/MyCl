e2a6333544574e2ac7a0e8a7a41ed69a179d222d
jxytherealone <thushar17223@gmail.com>
2023-09-11 15:01:06 +0530

    .

--- backend-php/student-login.php
+++ backend-php/student-login.php
@@ -1,11 +1,12 @@
 <?php 
 
     include('connect.php');
-    $email = $password = "";
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
         $email = $_POST['email'];
         $password = $_POST['password'];
+
         $sql = "SELECT email, password, id FROM registered_users WHERE email = ? AND password = ?";
         $stmt = $conn->prepare($sql);
 
@@ -16,12 +17,10 @@
         if ($result->num_rows === 1) {
             $row = $result->fetch_assoc();
             $id = $row['id'];
-            // User exists, redirect to home.html
             setcookie("id", $id, time() + 3600, "/");
             header('Location: ../../php-project/student-dashboard.php');
             exit();
         } else {
-            // User does not exist or wrong credentials
             echo "Invalid email or password.";
         }
 
