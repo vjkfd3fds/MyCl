de5efa032b40d0a99ecc3345845109f02c8b01dd
jxytherealone <thushar17223@gmail.com>
2023-09-19 22:16:50 +0530

    I don't remember what I did lol.......

--- college/college-dashboard.php
+++ college/college-dashboard.php
@@ -9,9 +9,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>MyCl | Dashboard</title>
     <link rel="stylesheet" href="style.css">
-    
+    <link rel="icon" href="../images/note.png">
 </head>
 <body>
     <!-- Navigation Bar -->
