b54f6c00fce286769b7f79f86e647886aa898fed
jxytherealone <thushar17223@gmail.com>
2023-09-20 16:26:12 +0530

    Fixed the overview part more than i expected pheww

---
+++ admin/overview.php
@@ -0,0 +1,50 @@
+<?php include_once "../backend-php/connect.php";?>
+
+<?php 
+
+    $stmt = $conn->prepare("SELECT * FROM registered_users");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyCl | Overview</title>
+    <link rel="icon" href="../images/note.png">
+    <style>
+        td, th, tr, table {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+    <h1>User Accounts</h1>
+    <table>
+        <tr>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Profile Picture</th>
+        </tr>
+
+        <?php 
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['firstname'] . "</td>";
+        ?>
+        <td> <img witdth="100" height="100" src="../profile/<?php echo $row['profile']; ?>" alt=""> </td>
+        <?php
+        }
+        ?>
+    </table>
+
+    
+</body>
+</html>
