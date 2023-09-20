7bbcb535bb8f71b0feda89c6c8517361000e8520
jxytherealone <thushar17223@gmail.com>
2023-09-20 18:51:38 +0530

    updated - settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -10,8 +10,12 @@
     <title>MyCl | settings</title>
 </head>
 <body>
-    <?php  
-          #can update password
-    ?>
+    <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Update password</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;">
+        <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Re-enter password</h1>
+        <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;"> <br />
+        <input type="submit" value="Update" style="margin-left: 50%; margin-top: 30px;">
+    </form>
 </body>
 </html>
