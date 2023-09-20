bc9023beb8da5360e1261b1bd2d4852e6755710e
jxytherealone <thushar17223@gmail.com>
2023-09-20 19:02:51 +0530

    i did something that i don't even know

--- admin/settings.php
+++ admin/settings.php
@@ -11,11 +11,21 @@
 </head>
 <body>
     <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Update password</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;">
         <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Re-enter password</h1>
         <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;"> <br />
-        <input type="submit" value="Update" style="margin-left: 50%; margin-top: 30px;">
+        <input type="submit" value="Update" style="margin-left: 50%; margin-top: 30px; font-size: 100px;" name="submit">
     </form>
+
+    <?php 
+        if (isset($_POST['submit'])) {
+            $pass = $_POST['password'];
+
+            $stmt = $conn->prepare("UPDATE password SET password = ? WHERE password = password");
+            $stmt->bind_param("s", $pass);
+            $stmt->execute();
+        }
+    ?>
 </body>
 </html>
