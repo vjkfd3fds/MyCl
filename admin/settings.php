48fc266f6caa86ed9e49c463db44695ce6bd99dd
jxytherealone <thushar17223@gmail.com>
2023-09-23 11:35:47 +0530

    new password stuffs

--- admin/settings.php
+++ admin/settings.php
@@ -1,31 +1,136 @@
-<?php include_once "../backend-php/connect.php"; ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="../images/note.png" />
-    <title>MyCl | settings</title>
-</head>
-<body>
-    <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Update password</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;">
-        <h1 style="font-family: monospace; text-align: center; margin-top: 5%;">Re-enter password</h1>
-        <input type="password" name="password" style=" font-size: 90px; margin-left: 13%; margin-top: 2%; font-family: monospace;"> <br />
-        <input type="submit" value="Update" style="margin-left: 50%; margin-top: 30px; font-size: 100px;" name="submit">
-    </form>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="../images/note.png">
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
 
-    <?php 
-        if (isset($_POST['submit'])) {
-            $pass = $_POST['password'];
+  <title>MyCl | Update Password</title>
 
-            $stmt = $conn->prepare("UPDATE password SET password = ? WHERE password = password");
-            $stmt->bind_param("s", $pass);
-            $stmt->execute();
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!-- font awesome style -->
+  <link href="../css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="../css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="../css/responsive.css" rel="stylesheet" />
+
+
+  <!-- Custom styles for the drop-down box -->
+  <style>
+    /* Style for the drop-down box */
+    .nav-item.dropdown {
+      position: relative;
+    }
+
+    .nav-link.dropdown-toggle::after {
+      display: none;
+    }
+
+    .dropdown-menu {
+      position: absolute;
+      top: 100%;
+      left: 0;
+      z-index: 1000;
+      display: none;
+      float: left;
+      min-width: 10rem;
+      padding: 0.5rem 0;
+      margin: 0;
+      font-size: 1rem;
+      color: #212529;
+      text-align: left;
+      list-style: none;
+      background-color: #fff;
+      background-clip: padding-box;
+      border: 1px solid rgba(0, 0, 0, 0.15);
+      border-radius: 0.25rem;
+    }
+
+    .nav-item.dropdown:hover .dropdown-menu {
+      display: block;
+    }
+    .custom-form {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
         }
-    ?>
+        .custom-form label {
+            font-weight: bold;
+        }
+        .feedback-list {
+            margin-top: 20px;
+        }
+
+        .feedback-card {
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 15px;
+            margin-bottom: 15px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .feedback-card h3 {
+            margin-top: 0;
+            margin-bottom: 10px;
+        }
+
+        .feedback-card p {
+            margin: 0;
+        }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+        <div class="custom-form">
+            <h1 style="text-align: center; padding: 25px;">Update Password</h1>
+            <form method="post" action="../backend-php/feedback.php">
+                <!-- Name input -->
+                <div class="form-group">
+                    <label for="form4Example1">New password</label>
+                    <input type="text" id="form4Example1" class="form-control" name="name"/>
+                </div>
+
+                <!-- Email input -->
+                <div class="form-group">
+                    <label for="form4Example2">Confirm password</label>
+                    <input type="email" id="form4Example2" class="form-control" name="email"/>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block" name="update">Update</button>
+            </form>
+        </div>
+    </div>
+
+  
+
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script src="../js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="../js/bootstrap.js"></script>
+  <!-- custom js -->
+  <script src="../js/custom.js"></script>
+
+
 </body>
+
 </html>
