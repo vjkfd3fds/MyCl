1c31fc8861304efe1747a12c59ffa16900c92ac2
jxytherealone <thushar17223@gmail.com>
2023-09-30 13:30:07 +0530

    .

--- students/details.php
+++ students/details.php
@@ -35,7 +35,9 @@ if (isset($_GET['institution'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyCl | College Details</title>
     <link rel="icon" href="../images/note.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
+        
         /* Add your CSS styles here */
         body {
             font-family: Arial, sans-serif;
@@ -103,6 +105,43 @@ if (isset($_GET['institution'])) {
             height: auto;
             margin: 10px;
         }
+
+        .rating:not(:checked) > input {
+            position: absolute;
+            appearance: none;
+        }
+
+            .rating:not(:checked) > label {
+            float: right;
+            cursor: pointer;
+            font-size: 30px;
+            color: #666;
+        }
+
+            .rating:not(:checked) > label:before {
+            content: '★';
+        }
+
+            .rating > input:checked + label:hover,
+            .rating > input:checked + label:hover ~ label,
+            .rating > input:checked ~ label:hover,
+            .rating > input:checked ~ label:hover ~ label,
+            .rating > label:hover ~ input:checked ~ label {
+            color: #e58e09;
+            }
+
+            .rating:not(:checked) > label:hover,
+            .rating:not(:checked) > label:hover ~ label {
+            color: #ff9e0b;
+            }
+
+            .rating > input:checked ~ label {
+            color: #ffa723;
+            }
+
+
+
+
     </style>
 </head>
 <body>
@@ -135,10 +174,53 @@ if (isset($_GET['institution'])) {
                 <h2>Images</h2>
                 <img src="../uploads/<?php echo $row['certificate']; ?>" alt="College Certificate">
             </div>
+            <div class="contact-info">
+                <h2 style="text-align: center;">Post a Review</h2>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+           Enter your review
+            </button>
 
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Reviews and Comments</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="rating">
+                        <input value="5" name="rate" id="star5" type="radio">
+                        <label title="text" for="star5"></label>
+                        <input value="4" name="rate" id="star4" type="radio">
+                        <label title="text" for="star4"></label>
+                        <input value="3" name="rate" id="star3" type="radio">
+                        <label title="text" for="star3"></label>
+                        <input value="2" name="rate" id="star2" type="radio">
+                        <label title="text" for="star2"></label>
+                        <input value="1" name="rate" id="star1" type="radio">
+                        <label title="text" for="star1"></label>
+                    </div>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">@</span>
+                        <input type="text" class="form-control" placeholder="Comment" aria-label="Username" aria-describedby="addon-wrapping">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Submit</button>
+                </div>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
     </form>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>
