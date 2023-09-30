b1f453d8faaa5bab1187b83cb4ff3e0d38ceb6a6
jxytherealone <thushar17223@gmail.com>
2023-09-30 11:05:32 +0530

    :<(

--- js/my-login.js
+++ js/my-login.js
@@ -1,19 +1,4 @@
-/******************************************
- * My Login
- *
- * Bootstrap 4 Login Page
- *
- * @author          Muhamad Nauval Azhar
- * @uri 			https://nauval.in
- * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
- * @license         My Login is licensed under the MIT license.
- * @github          https://github.com/nauvalazhar/my-login
- * @version         1.2.0
- *
- * Help me to keep this project alive
- * https://www.buymeacoffee.com/mhdnauvalazhar
- * 
- ******************************************/
+
 
 'use strict';
 
