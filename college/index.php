0b6cfae311cb77097e63cfeba92381d710f2b93d
jxytherealone <thushar17223@gmail.com>
2023-10-21 18:47:09 +0530

    fix

--- college/index.php
+++ college/index.php
@@ -13,6 +13,21 @@
 
 ?>
 
+<?php 
+	if (isset($_COOKIE['cid'])) {
+		$cid = $_COOKIE['cid'];
+
+	$sql1 = "SELECT * FROM college_details WHERE cid = '$cid'";
+	$result1 = $conn->query($sql1);
+	$row1 = $result1->fetch_assoc();
+    $institution = $row1['institution'];
+
+    $sql2 = "SELECT count(*) AS review_count FROM review WHERE institution = '$institution'";
+    $result2 = $conn->query($sql2);
+    $row2 = $result2->fetch_assoc();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
@@ -241,74 +256,13 @@
 				    <div class="col-6 col-lg-3">
 					    <div class="app-card app-card-stat shadow-sm h-100">
 						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Invoices</h4>
-							    <div class="stats-figure">6</div>
+							    <h4 class="stats-type mb-1">Reviews</h4>
+							    <div class="stats-figure"><?php echo $row2['review_count']; ?></div>
 							    <div class="stats-meta">New</div>
 						    </div><!--//app-card-body-->
 						    <a class="app-card-link-mask" href="#"></a>
 					    </div><!--//app-card-->
 				    </div><!--//col-->
-			    </div><!--//row-->
-			    <div class="row g-4 mb-4">
-			        <div class="col-12 col-lg-6">
-				        <div class="app-card app-card-chart h-100 shadow-sm">
-					        <div class="app-card-header p-3">
-						        <div class="row justify-content-between align-items-center">
-							        <div class="col-auto">
-						                <h4 class="app-card-title">Line Chart Example</h4>
-							        </div><!--//col-->
-							        <div class="col-auto">
-								        <div class="card-header-action">
-
-								        </div><!--//card-header-actions-->
-							        </div><!--//col-->
-						        </div><!--//row-->
-					        </div><!--//app-card-header-->
-					        <div class="app-card-body p-3 p-lg-4">
-							    <div class="mb-3 d-flex">   
-							        <select class="form-select form-select-sm ms-auto d-inline-flex w-auto">
-									    <option value="1" selected>This week</option>
-									    <option value="2">Today</option>
-									    <option value="3">This Month</option>
-									    <option value="3">This Year</option>
-									</select>
-							    </div>
-						        <div class="chart-container">
-				                    <canvas id="canvas-linechart" ></canvas>
-						        </div>
-					        </div><!--//app-card-body-->
-				        </div><!--//app-card-->
-			        </div><!--//col-->
-			        <div class="col-12 col-lg-6">
-				        <div class="app-card app-card-chart h-100 shadow-sm">
-					        <div class="app-card-header p-3">
-						        <div class="row justify-content-between align-items-center">
-							        <div class="col-auto">
-						                <h4 class="app-card-title">Bar Chart Example</h4>
-							        </div><!--//col-->
-							        <div class="col-auto">
-								        <div class="card-header-action">
-									    
-								        </div><!--//card-header-actions-->
-							        </div><!--//col-->
-						        </div><!--//row-->
-					        </div><!--//app-card-header-->
-					        <div class="app-card-body p-3 p-lg-4">
-							    <div class="mb-3 d-flex">   
-							        <select class="form-select form-select-sm ms-auto d-inline-flex w-auto">
-									    <option value="1" selected>This week</option>
-									    <option value="2">Today</option>
-									    <option value="3">This Month</option>
-									    <option value="3">This Year</option>
-									</select>
-							    </div>
-						        <div class="chart-container">
-				                    <canvas id="canvas-barchart" ></canvas>
-						        </div>
-					        </div><!--//app-card-body-->
-				        </div><!--//app-card-->
-			        </div><!--//col-->
-			        
 			    </div><!--//row-->
 			    <div class="row g-4 mb-4">
 				    <div class="col-12 col-lg-6">
