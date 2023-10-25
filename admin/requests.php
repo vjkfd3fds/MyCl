281afbdc561c96eac7b55389fa58258d7d7a68f6
jxytherealone <thushar17223@gmail.com>
2023-10-25 20:37:11 +0530

    fix: fixed typos

--- admin/requests.php
+++ admin/requests.php
@@ -34,122 +34,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyCl | Requests</title>
     <link rel="icon" href="../images/note.png">
-    <style>
-        .random-button {
-        display: inline-block;
-        padding: 10px 20px;
-        font-size: 16px;
-        background-color: #3498db;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-        feedback-list {
-            margin-top: 20px;
-        }
-
-        .feedback-card {
-            background-color: #fff;
-            border-radius: 5px;
-            padding: 15px;
-            margin-bottom: 15px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        .feedback-card h3 {
-            margin-top: 0;
-            margin-bottom: 10px;
-        }
-
-        .feedback-card p {
-            margin: 0;
-            font-family: monospace;
-            font-size: 15px;
-        }
-        /* Style for the result box */
-        .result-box {
-            border: 1px solid #ccc;
-            padding: 20px;
-            margin: 20px;
-            background-color: #f9f9f9;
-            border-radius: 10px;
-            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        /* Style for heading */
-        .result-heading {
-            font-size: 1.2rem;
-            margin-bottom: 10px;
-        }
-
-        /* Style for individual feedback card */
-        .feedback-card {
-            border: 1px solid #ddd;
-            padding: 10px;
-            margin: 10px 0;
-            background-color: #fff;
-            border-radius: 5px;
-        }
-
-        /* Style for feedback card content */
-        .feedback-content {
-            font-size: 0.9rem;
-        }
-
-        .imgLOL {
-            width: 400px;
-            height: 400px;
-            margin-left: 500px; /* Keep this as it is if you want horizontal spacing. */
-            margin-top: -16%; /* Move the image to the top. */
-        }
-
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1 style="font-family: monospace;">New-Requests</h1>
+    <div class="container mt-4">
+    <h1 class="text-center;">New-Requests</h1>
     <?php 
     include('../backend-php/connect.php');
 
     $sql = "SELECT * FROM college_details";
     $result = $conn->query($sql);
-
+    echo '<form action="requests.php" method="post">';
     if ($result->num_rows > 0) {
-        echo '<div class="result-box">';
-        
-        echo '<form action="requests.php" method="post">';
+        echo '<div class="card mb-3">';
         
         while ($row = $result->fetch_assoc()) {
             if ($row['status'] == 'unverified') {
-                echo '<div class="feedback-card">';
-                echo '<h3>' . $row["institution"] . '</h3>';
-                echo '<p style="font-size: 15px";> university: ' . $row["university"] . '</p>';
-                echo '<p style="font-size: 15px";> state: ' . $row["state"] . '</p>';
-                echo '<p style="font-size: 15px";> district: ' . $row["district"] . '</p>';
-                echo '<p style="font-size: 15px";> address: ' . $row["address"] . '</p>';
-                echo '<p style="font-size: 15px";> programs: ' . $row["programs"] . '</p>';
-                echo '<p style="font-size: 15px";> course: ' . $row["course"] . '</p>';
-                echo '<p style="font-size: 15px";> phone number: ' . $row["number"] . '</p>';
-                echo '<p style="font-size: 15px";> email: ' . $row["email"] . '</p>';
-                echo '<p style="font-size: 15px";> total seats: ' . $row["total_seats"] . '</p>';
-                echo '<p style="font-size: 15px";> reserved seats: ' . $row["reserved_seats"] . '</p>';
-                echo '<p style="font-size: 15px";> management_seats seats: ' . $row["management_seats"] . '</p>';
-                echo '<p style="font-size: 15px";> description: ' . $row["about"] . '</p>';
+                echo '<div class="card-body">';
+                echo '<h3 class="card-title">' . $row["institution"] . '</h3>';
+                echo '<ul class="list-group list-group-flush">';
+                echo '<li class="list-group-item"> university: ' . $row["university"] . '</li>';
+                echo '<li class="list-group-item"> state: ' . $row["state"] . '</li>';
+                echo '<li class="list-group-item"> district: ' . $row["district"] . '</li>';
+                echo '<li class="list-group-item"> address: ' . $row["address"] . '</li>';
+                echo '<li class="list-group-item"> programs: ' . $row["programs"] . '</li>';
+                echo '<li class="list-group-item"> course: ' . $row["course"] . '</li>';
+                echo '<li class="list-group-item"> phone number: ' . $row["number"] . '</li>';
+                echo '<li class="list-group-item"> email: ' . $row["email"] . '</li>';
+                echo '<li class="list-group-item"> total seats: ' . $row["total_seats"] . '</li>';
+                echo '<li class="list-group-item"> reserved seats: ' . $row["reserved_seats"] . '</li>';
+                echo '<li class="list-group-item"> management_seats seats: ' . $row["management_seats"] . '</li>';
+                echo '<li class="list-group-item"> description: ' . $row["about"] . '</li>';
                 ?>
-                <img src="../../MyCl/certificate/<?php echo $row['certificate']; ?>"class="imgLOL">
+                <img src="../../MyCl/certificate/<?php echo $row['certificate']; ?>" class="img-fluid mt-3" style="max-width: 300px;">
                 <?php
 
                 echo '<input type="hidden" name="user_ids[]" value="' . $row['cid'] . '">';
+                echo '<div class="mt-3 d-grid gap-2 d-md-block">';
+                echo '<input type="submit" value="Accept" name="pass" class="btn btn-success">';
+                echo '<input type="submit" value="Reject" name="reject" class="btn btn-danger">';
                 echo '</div>';
-                echo '<input type="submit" value="Accept" name="pass" class="random-button">';
-                echo '<input type="submit" value="Reject" name="reject" class="random-button">';
-                echo '</form>';
+                echo '</div>';
                 echo '</div>';
             } 
         }
         
     }
+    echo '</form>';
     $conn->close();
 ?>
 
